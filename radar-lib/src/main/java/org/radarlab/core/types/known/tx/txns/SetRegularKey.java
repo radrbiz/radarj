3e8eb98d15afc6e03484e76cc38a8070f03d378c
jameslee88 <leejamesleejames88@gmail.com>
2015-09-28 10:58:51 +0800

    add tx type: SetRegularKey

---
+++ radar-lib/src/main/java/org/radarlab/core/types/known/tx/txns/SetRegularKey.java
@@ -0,0 +1,30 @@
+package org.radarlab.core.types.known.tx.txns;
+
+
+import org.radarlab.core.AccountID;
+import org.radarlab.core.TxObj;
+import org.radarlab.core.fields.Field;
+import org.radarlab.core.serialized.enums.TransactionType;
+import org.radarlab.core.types.known.tx.Transaction;
+
+/**
+ * Created by wenfeng on 15/8/17.
+ */
+public class SetRegularKey extends Transaction {
+    public SetRegularKey() {
+        super(TransactionType.SetRegularKey);
+    }
+
+    public AccountID RegularKey() {return get(AccountID.RegularKey);}
+    public void RegularKey(AccountID val) {put(Field.RegularKey, val);}
+
+
+    @Override
+    public TxObj analyze(String address){
+        init();
+        item.setType("set_regular_key");
+        if(RegularKey()!=null)
+            item.setRecipient(RegularKey().address);
+        return item;
+    }
+}
